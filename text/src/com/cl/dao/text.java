6168aa7092b5ee04f6029079d8a980829a8fc8f8
DESKTOP-ACOF3D1\one <476275076@qq.com>
2018-04-19 11:13:24 +0800

    打印HELLOWORD

---
+++ text/src/com/cl/dao/text.java
@@ -0,0 +1,10 @@
+package com.cl.dao;
+
+/**
+ * Created by one on 2018/4/19.
+ */
+public class text {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+    }
+}
